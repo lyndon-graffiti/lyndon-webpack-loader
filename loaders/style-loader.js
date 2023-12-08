61c2a3e84e969df5e0571e68a8452f222c52ebc8
Lyndon <xianzhiwu@trip.com>
2023-12-08 18:54:13 +0800

    feat: style-loader

---
+++ loaders/style-loader.js
@@ -0,0 +1,10 @@
+// webpack 基于 node，所以使用 CommonJS 模块化
+// 打包过程不执行 JS，但是会进行 AST 分析，无法解析的代码，直接抛出错误
+module.exports = (sourceCode) => {
+  const code = `
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = \`${sourceCode}\`;
+    document.head.appendChild(styleElement);
+  `;
+  return code;
+};
